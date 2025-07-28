3bdcd3e4788597aba8670e6ac6bb02ef886348ab
erlina956 <erlinakovaqi@gmail.com>
2025-07-28 14:00:13 +0200

    Ndrushimi i trete

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const description1 = document.getElementById("description");
 const temp_max1 = document.getElementById("temp_max");
 const temp_min1 = document.getElementById("temp_min");
 const feelsLike1 = document.getElementById("feels_like");
-const humidity1 = document.getElementById("humindity"); 
+const humidity1 = document.getElementById("humindity");
 const wind1 = document.getElementById("wind");
 const pressure1 = document.getElementById("pressure");
 const visibility1 = document.getElementById("visibility");
 const img = document.getElementById("img");
-const container = document.getElementById("container");
-const map = document.getElementById("carouselslide");
+const container1 = document.getElementById("container1");
 const weather = document.getElementById("weather");
-const apiKey = "acea729982ae0eaf3c70c17f5a484aa7"; 
+
+const apiKey = "acea729982ae0eaf3c70c17f5a484aa7";
 
 searchBtn.addEventListener("click", () => {
   const city = cityInput.value.trim();
@@ -22,35 +22,33 @@ searchBtn.addEventListener("click", () => {
     alert("Please enter a city name.");
     return;
   }
-  getWeather(city); 
+  getWeather(city);
 });
 
 function getWeather(city) {
-
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-     .then(response => response.json())
-    .then((data) => {
-      const ikona = data.weather[0].icon;
-      const iconURL = `https://openweathermap.org/img/wn/${ikona}@2x.png`;
+    .then(response => response.json())
+    .then(data => {
+      const icon = data.weather[0].icon;
+      const iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
       img.style.display = "none";
-      container.style.background = "transparent";
-      map.style.display = "block";
+      container1.style.background = "transparent";
       weather.style.display = "block";
-      name1.innerHTML = `${data.name}  <img src="${iconURL}" alt="icon" width="100" hight="100px">`;
-      temp1.innerHTML = `${data.main.temp}°`;
+
+      name1.innerHTML = `${data.name} <img src="${iconURL}" alt="icon" width="100" height="100">`;
+      temp1.innerHTML = `${data.main.temp}°C`;
       description1.innerHTML = `${data.weather[0].description}`;
       temp_max1.innerHTML = `${data.main.temp_max}° /`;
       temp_min1.innerHTML = `${data.main.temp_min}°`;
-      feelsLike1.innerHTML = `Feels like  ${data.main.feels_like} °C`;
-      humidity1.innerHTML = `💧 ${data.main.humidity} % <br>   Humindity `;
-      wind1.innerHTML = `🌬️ ${data.wind.speed} m/s <br> Wind `;
-      pressure1.innerHTML = `📏  Pressure      ${data.main.pressure} mb`;
-      visibility1.innerHTML = `👁️  Visibility    ${data.visibility} m`;
+      feelsLike1.innerHTML = `Feels like ${data.main.feels_like} °C`;
+      humidity1.innerHTML = `💧 ${data.main.humidity}%<br>Humidity`;
+      wind1.innerHTML = `🌬️ ${data.wind.speed} m/s<br>Wind`;
+      pressure1.innerHTML = `📏 Pressure ${data.main.pressure} mb`;
+      visibility1.innerHTML = `👁️ Visibility ${data.visibility} m`;
     })
-    .catch((error) => {
+    .catch(error => {
       alert("Oops! Could not fetch any information about the weather.");
-      console.log(error);
+      console.error(error);
     });
 }
-
